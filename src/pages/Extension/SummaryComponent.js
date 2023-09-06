1c905e3ae04b93260d3cb8adb9a19b44f4ca92f8
Daniel Martinez <daniellhf@icloud.com>
2023-09-05 21:25:34 -0400

    okay title again

--- src/pages/Extension/SummaryComponent.js
+++ src/pages/Extension/SummaryComponent.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useContext } from 'react';
-import './ResultComponent.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import { ColorThemeContext } from './Extension';
+import React, { useEffect, useRef, useContext } from "react";
+import "./ResultComponent.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { ColorThemeContext } from "./Extension";
 
 const SummaryComponent = (props) => {
   const { dark } = useContext(ColorThemeContext);
@@ -11,19 +11,27 @@ const SummaryComponent = (props) => {
   return (
     <div
       style={{
-        width: '100%',
-        background: 'transparent',
+        width: "100%",
+        background: "transparent",
         minHeight: 100,
         borderRadius: 0,
-        boxSizing: 'border-box',
+        boxSizing: "border-box",
         padding: 15,
         ...props.style,
       }}
       className={`responseComponent ${props.className}`}
     >
-        <h4 className="summarized-response-title">Summarized Response</h4>
-      <div style={{ display: 'inline' }}>
-        <p style={{ color: '#598AEE', display: 'inline', fontFamily: 'CohereText !important'}}>{props.content}</p>
+      <h4 className="summarized-response-title">Summarized Response</h4>
+      <div style={{ display: "inline" }}>
+        <p
+          style={{
+            color: "#598AEE",
+            display: "inline",
+            fontFamily: "CohereText !important",
+          }}
+        >
+          {props.content}
+        </p>
       </div>
     </div>
   );
